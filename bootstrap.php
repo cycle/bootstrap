1ac92834c5b650dc036b48337a44be0cae68b5ce
Anton Titov <wolfy.jd@gmail.com>
2019-09-03 13:24:24 +0300

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -2,6 +2,10 @@
 // Cycle bootstrap
 declare(strict_types=1);
 
+use Doctrine\Common\Annotations\AnnotationRegisty;
+
 require_once "vendor/autoload.php";
+
+AnnotationRegistry::registerLoader('class_exists');
 
 $orm = \Cycle\Bootstrap\Bootstrap::fromConfigFile('config/cycle.php');
